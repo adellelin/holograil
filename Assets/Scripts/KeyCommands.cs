93cc8116ef030e1613a5918cc36a90c68013d682
Adelle Lin <adelle.lin@gmail.com>
2017-04-15 17:45:53 -0400

    Toni Dove Scene Working from vuforia scene to exit

--- Assets/Scripts/KeyCommands.cs
+++ Assets/Scripts/KeyCommands.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class KeyCommands : MonoBehaviour {
+
+    public Text ToniDoveBio;
 
     // called by gazegesturemanager
     public void OnSelect()
     {
-        SceneManager.LoadScene("SwitchTo");
+        SceneManager.LoadScene("ToniDove");
+        ToniDoveBio.text = "Toni Dove Bio ";
     }
 }
