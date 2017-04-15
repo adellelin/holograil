2905d0a97286d20f558efda38e23fd5b4a146f51
Adelle Lin <adelle.lin@gmail.com>
2017-04-15 18:08:23 -0400

    Working on lighting activation zone

---
+++ Assets/Scripts/DeskLighting.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeskLighting : MonoBehaviour {
+    public GameObject deskPosition;       //Public variable to store a reference to the player game object
+    public Light deskLightObject;
+    private Vector3 deskOffset;         //Private variable to store the offset distance between the player and camera
+   // public Text PositionCamera;
+
+    // Use this for initialization
+    void Start () {
+
+        deskLightObject.enabled = false;
+        //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+        //deskOffset = transform.localPosition - deskPosition.transform.localPosition;
+        //deskLightObject = new GameObject("TheLight");
+        //Light lightComp = deskLightObject.AddComponent<Light>();
+        //lightComp.color = Color.blue;
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider CollisionCube)
+    {
+        deskLightObject.enabled = true;
+
+    }
+
+    // LateUpdate is called after Update each frame
+    void LateUpdate()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        //deskLightObject.transform.localPosition = deskPosition.transform.localPosition + new Vector3(0, 2, 0);
+        //deskLightObject.transform.localPosition = new Vector3(0, 0, 0);
+        Debug.Log(transform.localPosition);
+       // PositionCamera.text = transform.localPosition.ToString();
+    }
+}
---
+++ Assets/Scripts/ExitZoneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitZoneManager : MonoBehaviour {
+
+    void Start()
+    {
+        Debug.Log("Logging Collision script");
+    }
+
+	void OnTriggerEnter(Collider CollisionCube)
+    {
+        Debug.Log("Collision Detected");
+        Renderer render = GetComponent<Renderer>();
+        render.material.color = Color.cyan;
+        SceneManager.LoadScene("VuforiaTest");
+
+    }
+
+
+}
