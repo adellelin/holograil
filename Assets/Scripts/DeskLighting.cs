ae427793b79ed33d704d2e259939de043a57710b
Adelle Lin <adelle.lin@gmail.com>
2017-04-15 23:13:06 -0400

    desk lighting turns on using distance calculation instead of trigger. Scene exit trigger confirmed working

--- Assets/Scripts/DeskLighting.cs
+++ Assets/Scripts/DeskLighting.cs
@@ -7,6 +7,7 @@ public class DeskLighting : MonoBehaviour {
     public GameObject deskPosition;       //Public variable to store a reference to the player game object
     public Light deskLightObject;
     private Vector3 deskOffset;         //Private variable to store the offset distance between the player and camera
+    private Vector3 viewerTransform;
    // public Text PositionCamera;
 
     // Use this for initialization
@@ -14,7 +15,9 @@ public class DeskLighting : MonoBehaviour {
 
         deskLightObject.enabled = false;
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        //deskOffset = transform.localPosition - deskPosition.transform.localPosition;
+        viewerTransform = transform.localPosition;
+        Debug.Log("viewerPosition: " + viewerTransform);
+        
         //deskLightObject = new GameObject("TheLight");
         //Light lightComp = deskLightObject.AddComponent<Light>();
         //lightComp.color = Color.blue;
@@ -23,14 +26,26 @@ public class DeskLighting : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        deskOffset = transform.localPosition - deskPosition.transform.localPosition;
+        //Debug.Log("DistanceFromDesk: " + deskOffset);
 
+        if (deskOffset.x < 2f)
+        {
+            deskLightObject.enabled = true;
+        } else
+        {
+            deskLightObject.enabled = false;
+        }
+	}
+    /*
     void OnTriggerEnter(Collider CollisionCube)
     {
         deskLightObject.enabled = true;
+        GameObject otherObj = CollisionCube.gameObject;
+        Debug.Log("Triggered with: " + otherObj);
 
     }
+    */
 
     // LateUpdate is called after Update each frame
     void LateUpdate()
@@ -38,7 +53,7 @@ public class DeskLighting : MonoBehaviour {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
         //deskLightObject.transform.localPosition = deskPosition.transform.localPosition + new Vector3(0, 2, 0);
         //deskLightObject.transform.localPosition = new Vector3(0, 0, 0);
-        Debug.Log(transform.localPosition);
+        //Debug.Log(transform.localPosition);
        // PositionCamera.text = transform.localPosition.ToString();
     }
 }
--- Assets/Scripts/ExitZoneManager.cs
+++ Assets/Scripts/ExitZoneManager.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ExitZoneManager : MonoBehaviour {
 
+    public Text exitText;
     void Start()
     {
         Debug.Log("Logging Collision script");
@@ -12,10 +14,12 @@ public class ExitZoneManager : MonoBehaviour {
 
 	void OnTriggerEnter(Collider CollisionCube)
     {
-        Debug.Log("Collision Detected");
+        Debug.Log("Went through the exit");
         Renderer render = GetComponent<Renderer>();
+        
         render.material.color = Color.cyan;
         SceneManager.LoadScene("VuforiaTest");
+        exitText.text = "exited";
 
     }
 
